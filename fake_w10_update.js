1deab5d411e1bf65221b8a1ef39aa24824a69794
BeBoXoS <33153857+beboxos@users.noreply.github.com>
2019-01-23 11:43:57 +0100

    fake upadate
    little joke with a fake win10 update

---
+++ fake_w10_update.js
@@ -0,0 +1,21 @@
+// fake update LUL
+// author: Judge2020 https://judge2020.com
+// edited by: DanielL99 on github (thanks!)
+//
+// conversion by BeBoX for P4wnP1 A.L.O.A
+//
+layout('fr');			// FR keyboard layout
+typingSpeed(0,0);	//fast
+press("GUI r");
+delay(500);
+type("iexplore -k http://fakeupdate.net/win10u/index.html\n")
+delay(5000);
+press("GUI r");
+delay(500);
+type("notepad\n");
+delay(1000);
+type("You could have been pawned by fake update ;-)\nDon't leave your computer alone !");
+delay(500);
+press("ALT TAB");
+delay(5000);
+press("ALT F4");
