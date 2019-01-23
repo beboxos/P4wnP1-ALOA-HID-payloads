59ee7237525daddaa04bea4ff7cf7481941250e4
BeBoXoS <33153857+beboxos@users.noreply.github.com>
2019-01-23 11:40:05 +0100

    basic run as admin

---
+++ powershell as Admin.js
@@ -0,0 +1,10 @@
+// Blank payload 
+layout('fr');			// FR keyboard layout
+typingSpeed(0,0)        // FAST typingSpeed
+press("GUI r")			// Run Command
+delay(200)				// wait a little
+type("powershell")			// call command
+press("CTRL SHIFT ENTER")	// run as administrator
+delay(1000)				// wait a little
+press("SHIFT TAB")		// select Yes 
+press("ENTER")			// validate
